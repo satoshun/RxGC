3cc5d4c36eb18feb8d5af0474758f86dfcef750c
SatoShun <shun.sato1@gmail.com>
2016-11-05 01:46:55 +0000

    watch multi items

--- app/src/main/java/com/github/satoshun/reactive/gc/example/MainActivity.java
+++ app/src/main/java/com/github/satoshun/reactive/gc/example/MainActivity.java
@@ -6,7 +6,10 @@ import android.util.Log;
 
 import com.github.satoshun.reactive.gc.RxGC;
 
-import io.reactivex.functions.Action;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,24 +23,41 @@ public class MainActivity extends AppCompatActivity {
 
     RxGC.watch(target)
             .subscribeOn(Schedulers.io())
-            .subscribe(new Action() {
-              @Override
-              public void run() throws Exception {
-                Log.d("RxGC", "GCed");
-              }
-            });
+            .subscribe(() -> Log.d("RxGC single", "GCed"));
 
-    new Thread(new Runnable() {
-      @Override
-      public void run() {
-        try {
-          Thread.sleep(3000);
-        } catch (InterruptedException e) {
-          e.printStackTrace();
-        }
-        // release object
-        target = null;
+    new Thread(() -> {
+      try {
+        Thread.sleep(3000);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
       }
+      // release object
+      target = null;
+    }).start();
+
+    testMultiWatch();
+  }
+
+  private List<String> target1 = new ArrayList<>();
+  private List<String> target2 = new LinkedList<>();
+
+  private void testMultiWatch() {
+    RxGC.watch(target1, target2)
+            .subscribeOn(Schedulers.io())
+            .subscribe(
+                    ref -> Log.d("RxGC multi next", String.valueOf(ref.get())),
+                    e -> Log.e("RxGC multi error", e.getMessage()),
+                    () -> Log.d("RxGC multi complete", "complete"));
+
+    new Thread(() -> {
+      try {
+        Thread.sleep(3000);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+      // release object
+      target1 = null;
+      target2 = null;
     }).start();
   }
 }
--- rxgc/src/main/java/com/github/satoshun/reactive/gc/RxGC.java
+++ rxgc/src/main/java/com/github/satoshun/reactive/gc/RxGC.java
@@ -4,10 +4,15 @@ package com.github.satoshun.reactive.gc;
 import java.lang.ref.Reference;
 import java.lang.ref.ReferenceQueue;
 import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import io.reactivex.Completable;
-import io.reactivex.CompletableEmitter;
-import io.reactivex.CompletableOnSubscribe;
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
 
 public class RxGC {
 
@@ -17,21 +22,38 @@ public class RxGC {
 
   public static Completable watch(Object target) {
     ReferenceQueue<Object> queue = new ReferenceQueue<>();
-    return watch(new WeakReference<>(target, queue), queue);
+    Observable<Reference<Object>> observable = internalWatch(Collections.singletonList(new WeakReference<>(target, queue)), queue);
+    return Completable.fromObservable(observable);
   }
 
-  private static Completable watch(final Reference<?> target,
-                                   final ReferenceQueue<Object> queue) {
-    return Completable.create(new CompletableOnSubscribe() {
+  public static <T> Observable<Reference<T>> watch(T target, T... targets) {
+    ReferenceQueue<T> queue = new ReferenceQueue<>();
+    ArrayList<WeakReference<T>> references = new ArrayList<>();
+    references.add(new WeakReference<>(target, queue));
+    for (T t : targets) {
+      references.add(new WeakReference<>(t, queue));
+    }
+    return internalWatch(references, queue);
+  }
+
+  private static <T> Observable<Reference<T>> internalWatch(final List<? extends Reference<T>> targets,
+                                                            final ReferenceQueue<T> queue) {
+    return Observable.create(new ObservableOnSubscribe<Reference<T>>() {
+
       // avoids GC
-      private final Reference<?> reference = target;
+      private final List<? extends Reference<T>> references = targets;
+      private final AtomicInteger count = new AtomicInteger();
 
       @Override
-      public void subscribe(CompletableEmitter e) throws Exception {
+      public void subscribe(ObservableEmitter<Reference<T>> e) throws Exception {
         while (true) {
-          Reference<?> ref = queue.poll();
+          Reference<? extends T> ref = queue.poll();
           if (ref != null) {
-            break;
+            e.onNext((Reference<T>) ref);
+            if (count.incrementAndGet() >= targets.size()) {
+              break;
+            }
+            continue;
           }
           Thread.sleep(DEFAULT_WAIT_MS);
           if (IS_DEBUG) {
